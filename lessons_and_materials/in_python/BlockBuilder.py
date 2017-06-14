74d16626e21d250ca139eac755a887de4a2aa099
Davis Mohar <davismohar@gmail.com>
2017-06-14 10:50:40 -0400

    Revert "Added Python Code"
    This reverts commit c90f3394af08d94aed504f4c2528dd93133ab3bf.

--- lessons_and_materials/in_python/BlockBuilder.py
+++
@@ -1,9 +0,0 @@
-import mcpi.minecraft as minecraft
-mc = minecraft.Minecraft.create()
-
-import mcpi.block as block
-
-pos = mc.player.getTilePos()
-
-for y in range(150):
-    mc.setBlock(pos.x+2,pos.y+y,pos.z,block.TNT.id)
